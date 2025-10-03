e8080b535f8f69a3cf1510cdabb2f3c7e6ddde99
Shadoww111 <pedro.shadow.costa@gmail.com>
2025-10-03 20:30:00 +0100

    user model

---
+++ server/models/User.js
@@ -0,0 +1,11 @@
+const { DataTypes } = require("sequelize");
+const { sequelize } = require("../config/db");
+const bcrypt = require("bcryptjs");
+const User = sequelize.define("User", {
+  id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+  username: { type: DataTypes.STRING(50), allowNull: false, unique: true },
+  email: { type: DataTypes.STRING(100), allowNull: false, unique: true, validate: { isEmail: true } },
+  password: { type: DataTypes.STRING(255), allowNull: false },
+}, { tableName: "users", hooks: { beforeCreate: async (u) => { u.password = await bcrypt.hash(u.password, 12); } } });
+User.prototype.checkPw = async function (pw) { return bcrypt.compare(pw, this.password); };
+module.exports = User;
